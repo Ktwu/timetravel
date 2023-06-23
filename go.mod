58b876d58a676ec87e3f5c409579e94beb21936c
Kellie Medlin <kellie.medlin@gmail.com>
2023-06-23 15:06:30 -0700

    Refactor: in memory records
    In preparation for testing a more complicated record schema involving
    sqlite, let's break out the recordkeeping into new files and add some
    basic sanity testing (that can be refactored for common use later, once
    sqlite is hooked in)

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/temelpa/timetravel
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/google/go-cmp v0.5.9
+	github.com/gorilla/mux v1.8.0
+)
---
+++ service/in_memory_record_test.go
@@ -0,0 +1,81 @@
+package service
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/temelpa/timetravel/entity"
+)
+
+// Test basic read + write functionality
+func TestSanity(t *testing.T) {
+	service := NewInMemoryRecordService()
+
+	testEntity := entity.Record{
+		ID: 42,
+		Data: map[string]string{
+			"hello": "world",
+		},
+	}
+
+	testEntityUpdate := entity.Record{
+		ID: testEntity.ID,
+		Data: map[string]string{
+			"hello": "world",
+			"goodbye": "world",
+		},
+	}
+
+	testEntityUpdate2 := entity.Record{
+		ID: testEntity.ID,
+		Data: map[string]string{
+			"goodbye": "unittest",
+		},
+	}
+
+	// TODO: read up on contexts to understand what should
+	// actually go here
+	ctx := context.Background()
+
+	// Make sure the system is empty
+	if _, err := service.GetRecord(ctx, testEntity.ID); err != ErrRecordDoesNotExist {
+		t.Errorf("Should have failed grabbing nonexistant entry")
+	}
+	if service.CreateRecord(ctx, testEntity) != nil {
+		t.Errorf("Unable to create record for %v", testEntity)
+	}
+
+	// Attempting to create the same record again ought to fail
+	// as well as not mutate the original data
+	if service.CreateRecord(ctx, testEntityUpdate) != ErrRecordAlreadyExists {
+		t.Errorf("Erroneously created second conflicting record %v", testEntityUpdate)
+	}
+
+	// Test fetching the data and data integrity
+	if r, err := service.GetRecord(ctx, testEntity.ID); err != nil {
+		t.Errorf("Unable to fetch record for %v", testEntity)
+	} else if !cmp.Equal(r, testEntity) {
+		t.Errorf("Fetched entry %v not the same as %v", r, testEntity)
+	}
+
+	// Test adding data
+	testValue := "world"
+	if r, err := service.UpdateRecord(ctx, testEntity.ID, map[string]*string {
+		"goodbye": &testValue,
+	}); err != nil {
+		t.Errorf("Unable to update record %v", testEntity)
+	} else if testValue = "unittest"; !cmp.Equal(r, testEntityUpdate) {
+		t.Errorf("Update entry %v not the same as %v", r, testEntityUpdate)
+	}
+
+	// Test mutating and removing data
+	if r, err := service.UpdateRecord(ctx, testEntity.ID, map[string]*string {
+		"hello": nil,
+		"goodbye": &testValue,
+	}); err != nil {
+		t.Errorf("Unable to update record %v", testEntity)
+	} else if !cmp.Equal(r, testEntityUpdate2) {
+		t.Errorf("Update entry %v not the same as %v", r, testEntityUpdate2)
+	}
+}
